c0973554f27c02febbab4ad648a77b31d10d3528
ra-js-epam-training <ruslan_afanasiev@epam.com>
2020-12-09 21:36:47 +0000

    [TASK-02] Added a new func to make a team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,23 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+/*
+* function returns name of a newly made team (string)
+* based on the names of its members (Array).
+*
+* Names of the members should be strings.
+* Values with other type should be ignored.
+* In case of wrong members type function must return false.
+*/
+module.exports = function createDreamTeam(members) {
+  // if there is a wrong input type or it's an empty array - return false
+  if (!Array.isArray(members) || members.length == 0) {
+    return false;
+  }
+  // to clean up input args and cast all names to uppercase
+  let membersPure = members
+    .filter(item => typeof item === 'string')
+    .map(item => item.trim().toUpperCase());
+  // to get first letters for all the items
+  return membersPure.map(item => item[0]).sort().join('');
 };
+
