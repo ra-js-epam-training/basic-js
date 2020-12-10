f22c396cd948ab6eb845b84e14796dab23cbd6ed
ra-js-epam-training <ruslan_afanasiev@epam.com>
2020-12-10 22:29:37 +0000

    [TASK-04] Added a new func to calculate C14 dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
+const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
+const LN_OF_TWO = 0.693;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  // exit if there are no arguments or the input is not a string
+  if (arguments.length === 0 || typeof sampleActivity !== "string") {
+    return false;
+  }
+  // the string must have numbers only
+  let sampleNum = new Number(sampleActivity);
+  if (typeof sampleNum !== 'number' || sampleNum <= 0 || sampleNum > MODERN_ACTIVITY ) {
+    return false;
+  }
+  // calculate the result and round up it
+  return ((sampleNum/LN_OF_TWO) * HALF_LIFE_PERIOD).toFixed();
 };
