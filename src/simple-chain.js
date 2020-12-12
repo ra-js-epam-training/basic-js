17a77ef4e2906710200e8ae3aa8fcff0f0ad2494
ra-js-epam-training <ruslan_afanasiev@epam.com>
2020-12-12 21:27:23 +0000

    [TASK-05] Added a new class to build a simple chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,33 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
-  addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value = ' ') {
+    this.chain.push(value);
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (position < 1 || position > this.getLength()) {
+      throw new CustomError('Wrong position!');
+    }
+    this.chain.splice(position - 1, 1);
+    return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    const chainStr = this.toString();
+    // to empty the array
+    this.chain.length = 0;
+    return chainStr;
+  },
+  toString() {
+    return this.chain.map(i => `( ${i} )`).join('~~');
   }
 };
 
