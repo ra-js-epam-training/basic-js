1a983ec5073d44297c9f6859f56934f8685b0ac5
estars-tv <maslivetsanatolii@gmail.com>
2020-12-16 18:19:58 +0200

    review

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,13 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function countCats(cats) {
+  //вариант решения
+  //const arr = [].concat(...cats);
+
+  //return arr.filter(str => str === '^^').length;
+
   let total = 0;
-  if (!Array.isArray(cats) || cats.length == 0) {
+  if (!Array.isArray(cats) || cats.length == 0) { //  предпочтительно строгое сравнение; можно обойтись без проверки на длину массива
     return total;
   }
 
   for (const element of cats) {
-    if (!Array.isArray(element) || element.length == 0) {
+    if (!Array.isArray(element) || element.length == 0) { // ===; так как выше уже была подобная проверка и вызвано возвращение результата, нет необходимости перепроверять это в цикле
       break;
     }
     total += element.reduce(function(sum, current) {
