a04ec3fb0a2afc7069cfb0f09e397e70e71e8a28
ra-js-epam-training <ruslan_afanasiev@epam.com>
2020-12-16 14:24:36 +0000

    [TASK-06] Code clean up

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,26 +1,5 @@
 const CustomError = require("../extensions/custom-error");
 
-// a basic class-like function
-/*
-function Text() {
-  this.txt = [];
-  this.str = '';
-  this.sep = '';
-};
-Text.prototype.repeat = function(num) {
-  for (let i = 0; i < num; i++) {
-    this.txt.push(this.str);
-  }
-};
-Text.prototype.separate = function() {
-  for (let i = 1; i < this.txt.length; i++) {
-    this.txt[i] = `${this.sep}${this.txt[i]}`;
-  }
-}
-Text.prototype.flatten = function() {
-  return this.txt.join('');
-};
-*/
 class Text {
   constructor(str, sep) {
     this.txt = [];
@@ -39,21 +18,12 @@ class Text {
       this.txt[i] = `${this.sep}${this.txt[i]}`;
     }
   }
+  // to convern an inner array into a string and return
   flatten() {
     return this.txt.join('');
   }
 }
 
-/*
-function Str(str, sep = '+') {
-  this.str = str;
-  this.sep = new String(sep);
-}
-Str.prototype = new Text();
-Str.prototype.addition = function(addObj) {
-  this.txt = this.txt.map(s => s + addObj.flatten());
-};
-*/
 // this is a representation of a string
 class Str extends Text {
   constructor(str, sep = '+') {
@@ -63,19 +33,6 @@ class Str extends Text {
     this.txt = this.txt.map(s => s + addObj.flatten());
   }
 }
-
-/*
-const str = new Str('foo', undefined);
-str.repeat(5);
-str.separate();
-console.log(str.txt);
-console.log(str.flatten());
-const add = new Addition('XXX', '00');
-add.repeat(3);
-add.separate();
-str.addition(add);
-console.log(str.flatten());
-*/
 
 module.exports = function repeater(str, options) {
   const strObj = new Str(str, options["separator"]);
